2fc6d6be6cbd25becaa0a329b29a6f0f94660f05
itscopebd <itscopebd@gmail.com>
2023-05-28 22:11:45 +0600

    Modify all files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 
 const loadMeal = document.getElementById("loadMeal");
 
-const loadMeals = () => {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=fish")
+const loadMeals = (searchText) => {
+    loadMeal.innerHTML="";
+    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
         .then(res => res.json())
         .then(data => showMeals(data.meals))
+        .catch(error=>{
+console.log(error)
+        })
 
 }
 
@@ -20,7 +25,7 @@ const showMeals = (data => {
             <div class="card-body">
               <h5 class="card-title">${strTags}</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalbox" onclick="showDetails(${idMeal})">
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalbox" onclick="showDetails2(${idMeal})">
              Details
               </button>
             </div>
@@ -30,13 +35,27 @@ const showMeals = (data => {
     });
 });
 
-const showDetails=(idMeal)=>{
+// const showDetails=(idMeal)=>{
 
- const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+//  const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
 
-fetch(url)
-.then(res=> res.json())
-.then(data=>showDataModelBox(data.meals))
+// fetch(url)
+// .then(res=> res.json())
+// .then(data=>showDataModelBox(data.meals))
+// }
+
+
+
+const showDetails2= async (idMeal)=>{
+    const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+try{
+    const res= await fetch(url);
+const data= await res.json();
+showDataModelBox(data.meals)
+}
+catch(error){
+
+}
 }
 
 
@@ -63,5 +82,15 @@ modal_body.innerHTML=`
 }
 
 
-loadMeals()
+// search product 
+
+const searchText=()=>{
+    const searchValue=document.getElementById("searchField").value;
+    loadMeals(searchValue);
+}
+
+
+
+loadMeals('fish')
+
 
